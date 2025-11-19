e19b312d426511e869022508431a1e5125696026
chrismelville29 <cmelville113@gmail.com>
2025-11-19 14:46:00 -0600

    implementing tanh

--- jaxnn/jaxnn/nn/activations/tanh.py
+++ jaxnn/jaxnn/nn/activations/tanh.py
@@ -1,1 +1,17 @@
-"""placeholder for tanh
+"""tanh activation."""
+from __future__ import annotations
+
+import jax.numpy as jnp
+
+from .. import types
+
+Array = types.Array
+
+
+def tanh(x: Array) -> Array:
+    positive_exp = jnp.exp(x)
+    negative_exp = jnp.exp(-1 * x)
+    return (positive_exp - negative_exp) / (positive_exp + negative_exp)
+
+
+__all__ = ["tanh"]
