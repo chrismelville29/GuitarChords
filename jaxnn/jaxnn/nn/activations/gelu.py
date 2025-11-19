75836cdd1117eec5b00303f58ea474b175740d5e
chrismelville29 <cmelville113@gmail.com>
2025-11-19 15:22:04 -0600

    gelu

--- jaxnn/jaxnn/nn/activations/gelu.py
+++ jaxnn/jaxnn/nn/activations/gelu.py
@@ -1,1 +1,17 @@
-"""placeholder for gelu"""
+"""gelu activation."""
+from __future__ import annotations
+
+import jax.numpy as jnp
+
+from .. import types
+from .tanh import tanh
+
+Array = types.Array
+
+
+def gelu(x: Array) -> Array:
+    tanh_input = jnp.sqrt(2 / jnp.pi) * (x + 0.044715 * x ** 3)
+    return 0.5 * x * (1 + tanh(tanh_input))
+
+
+__all__ = ["gelu"]
